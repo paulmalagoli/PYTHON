1e4e471ed4e1deb21229ee93dd25360e2fbd2886
paulmalagoli <paul.malagoli@gmail.com>
2022-09-13 22:29:58 +0200

    kaggle

--- SITE/test.js
+++
@@ -1,8 +0,0 @@
-function foo() {
-  if (true) {
-    console.log("Hello world");
-    for (i = 0; i > 5; i++) {
-      console.log(i);
-    }
-  }
-}
--- Test python/ip_adresses.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import Ether, IP
- 
-mac = Ether().src
-ip = IP(dst="0.0.0.0").src
- 
-print("Mac: "+mac)
-print("IP: "+ip)
